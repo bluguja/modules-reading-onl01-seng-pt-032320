128f4119e13ab64b53a10fe06dd2bc46637f3264
Bala Luguja <bluguja@gmail.com>
2020-04-29 08:40:26 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,17 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-# class Kid
+class Kid
   
-#   extend MetaDancing
-#   include Dance 
+  extend MetaDancing
+  include Dance 
   
-#   attr_accessor :name 
+  attr_accessor :name 
   
-#   def initialize(name)
-#     @name = name
-#   end
-# end 
+  def initialize(name)
+    @name = name
+  end
+end 
 
 class Kid
   include Dance
